cb3ee444875ed7432bc87a1331d304eb4b6c1b48
Rodrigue <Rodriguekayembe@gmail.com>
2019-04-05 15:00:16 +0100

    rubocop linting

--- lib/shop.rb
+++ lib/shop.rb
@@ -1,22 +1,21 @@
-class Shop 
-    attr_reader :inventory
-    def initialize
-        @inventory = {'A' => 50, 'B' => 30 , 'C' => 20 , 'D' => 15 }
+class Shop
+  attr_reader :inventory
+  def initialize
+    @inventory = { 'A' => 50, 'B' => 30, 'C' => 20, 'D' => 15 }
+  end
+
+  def checkout(item)
+    if item.class == String && item == item.upcase
+      items = item.split('')
+      total = 0
+      items.each do |item|
+        total += @inventory[item]
+      end
+      total
+    else
+      -1
+    end
     end
 
-    def checkout(item)
-        if item.class == String && item == item.upcase
-            items = item.split('')
-            total = 0
-            items.each do |item| 
-                total += @inventory[item]
-            end
-            total
-        else
-            -1 
-        end
-      end
-
-      private
-    
+  private
 end
